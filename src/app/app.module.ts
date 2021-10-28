f564e9d86210321fdcafa7330f990834d97db8dd
MoodyJW <jmoody1813@gmail.com>
2021-10-28 12:14:07 -0500

    removed material because fuck you that's why

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +16,9 @@ import { AboutPageComponent } from './components/about-page/about-page.component
     HomePageComponent,
     ProjectsPageComponent,
     AboutPageComponent,
+    NavbarComponent,
   ],
-  imports: [
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    BrowserModule,
-    AngularMaterialModule,
-  ],
+  imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
