3d8c6a9e2d82de45e7bc87993c0840b015a15f8b
MoodyJW <jmoody1813@gmail.com>
2021-10-30 14:59:51 -0500

    I went too far! Nav is pretty solid, added a ton of other shit, even some content on about and a version switcher

--- src/app/components/about-page/about-page.component.ts
+++ src/app/components/about-page/about-page.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  transition,
+  style,
+  query,
+  animate,
+  AnimationQueryOptions,
+  state,
+} from '@angular/animations';
+
+export const optional: AnimationQueryOptions = { optional: true };
+
+export const fader = trigger('fader', [
+  transition('* <=> *', [
+    query(
+      ':enter, :leave',
+      [
+        style({
+          position: 'absolute',
+          left: 0,
+          padding: '1em',
+          width: '100%',
+          height: '100%',
+          opacity: 0,
+          transform: 'scale(0) translateY(100%)',
+        }),
+      ],
+      optional
+    ),
+    query(
+      ':enter',
+      [
+        animate(
+          '600ms ease',
+          style({
+            opacity: 1,
+            transform: 'scale(1) translateY(0)',
+          })
+        ),
+      ],
+      optional
+    ),
+  ]),
+]);
+
+const showHide = trigger('showHide', [
+  state('closed', style({ height: '0px', overflow: 'hidden' })),
+  state('open', style({ height: '*', overflow: 'hidden' })),
+  transition('open <=> closed', animate('900ms ease-in-out')),
+]);
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.scss']
+  styleUrls: ['./about-page.component.scss'],
+  animations: [fader, showHide],
 })
 export class AboutPageComponent implements OnInit {
+  short = true;
+  sectionIsOpen = [...Array(4)].map(() => 'closed');
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleShowHide(index: number): void {
+    this.sectionIsOpen[index] =
+      this.sectionIsOpen[index] === 'closed' ? 'open' : 'closed';
   }
-
 }
