220f5dd88eb59286af5fc862ca3560c818876f9b
MoodyJW <jmoody1813@gmail.com>
2021-11-17 17:05:57 -0600

    created email button, changed some styles to work with it, changed line height in proj cards

---
+++ src/app/components/contact-button/contact-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-button',
+  templateUrl: './contact-button.component.html',
+  styleUrls: ['./contact-button.component.scss'],
+})
+export class ContactButtonComponent {}
