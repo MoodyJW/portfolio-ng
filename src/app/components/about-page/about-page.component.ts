a06dba1ccfc0ce4f3d86e3fde0b547bcb8207842
MoodyJW <jmoody1813@gmail.com>
2021-11-18 16:02:57 -0600

    nav, home, and about styles  seem good

--- src/app/components/about-page/about-page.component.ts
+++ src/app/components/about-page/about-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { fader, showHide, rotatedState } from '../../component-animations';
 import { ABOUT_SECTIONS } from './about.constants';
 
@@ -8,13 +8,30 @@ import { ABOUT_SECTIONS } from './about.constants';
   styleUrls: ['./about-page.component.scss'],
   animations: [fader, showHide, rotatedState],
 })
-export class AboutPageComponent {
-  short = true;
+export class AboutPageComponent implements OnInit {
   sections = ABOUT_SECTIONS;
+  short = true;
+  innerWidth: number | undefined;
   sectionIsOpen = [...Array(this.sections.length)].map(() => 'closed');
   rotatedState = [...Array(this.sections.length)].map(() => 'default');
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+  }
+
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+
   toggleShowHide(index: number): void {
+    if (this.innerWidth && this.innerWidth > 1540) {
+      this.rotatedState[index] =
+        this.rotatedState[index] === 'rotated' ? 'default' : 'rotated';
+      this.sectionIsOpen[index] =
+        this.sectionIsOpen[index] === 'open' ? 'closed' : 'open';
+      return;
+    }
     this.rotatedState.forEach((rotatedState, i) => {
       if (index === i) {
         this.rotatedState[i] =
